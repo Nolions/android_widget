9034ade2e2d3748a3a59ac7227837c8a2d02ab31
NolionsChen <nolionschen@jetfuel.com.tw>
2020-09-14 15:15:27 +0800

    [feature] Second Widget

---
+++ app/src/main/java/tw/nolions/widget/SecondAppWidget.kt
@@ -0,0 +1,36 @@
+package tw.nolions.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class SecondAppWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            updateSecondAppWidget(context, appWidgetManager, appWidgetId)
+        }
+
+    }
+}
+
+internal fun updateSecondAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.second_app_widget)
+    views.setTextViewText(R.id.appwidget_text, "$appWidgetId")
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
