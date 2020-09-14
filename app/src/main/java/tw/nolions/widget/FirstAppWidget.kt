fb06ff1672a1088e2d1ff9bf447ed3ea24caa8ea
NolionsChen <nolionschen@jetfuel.com.tw>
2020-09-14 15:14:20 +0800

    [feature] first widget

---
+++ app/src/main/java/tw/nolions/widget/FirstAppWidget.kt
@@ -0,0 +1,35 @@
+package tw.nolions.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class FirstAppWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.first_app_widget)
+    views.setTextViewText(R.id.appwidget_text, context.getString(R.string.appwidget_text))
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
